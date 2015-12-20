fec071376ac2a662aea04764a12382ba8cbeb7eb
michal j wallace <michal.wallace@gmail.com>
2015-12-20 07:22:37 +0000

    remove embedded spaces from model.

--- elixtris/elixtris.exs
+++ elixtris/elixtris.exs
@@ -4,35 +4,36 @@ defmodule Elixtris do
   # type Cmd = state -> state
   # 's0' refers to the game state at start of the function
 
-  def empty_row, do: '. . . . . . . . . .'
   def initial_state do
     %{ matrix: for _ <- 0..21 do empty_row end,
        score: 0,
        numLines: 0,
-       tetramino: ['. . . . ', 'c c c c ', '. . . . ', '. . . . ']}
+       tetramino: ['....', 'cccc', '....', '....'] }
   end
 
-  # print :: Cmd
-  # print the state of the matrix
+  # -- helper routines ---
 
-  def print(s0) do
-    for line <- s0.matrix, do: IO.puts line
-    s0
-  end
+  def empty_row, do: '..........'
 
-  # print_tetramino :: Cmd
-  # print the state of the active tetramino
+  # respace :: str -> str  -- add a space after each character.
+  def respace(''), do: ''
+  def respace([h|t]), do: [h|[32|respace t]]
 
-  def print_tetramino(s0) do
-    Enum.map(s0.tetramino, &IO.puts/1)
-    s0
+  # print grid -> ()
+  def print(grid) do
+    for row <- grid, do: row |> respace |> IO.puts
   end
 
   # given :: Cmd
   # reads the matrix from stdin
 
   def given(s0) do
-    %{ s0 | matrix: for _ <- 0..21 do IO.gets("") |> String.rstrip |> String.to_char_list end }
+    %{ s0 | matrix:
+              for _ <- 0..21 do
+                IO.gets("")
+                |> String.replace(" ", "")
+                |> String.to_char_list
+              end }
   end
 
   # query :: state, [char] -> [char]
@@ -67,18 +68,18 @@ defmodule Elixtris do
 
   def io(s0, [ch|buf]) do
     s1 = case [ch] do
-           'p' -> print s0
+           'p' -> print s0.matrix; s0
            'g' -> given s0
            'c' -> initial_state
            's' -> step s0
            '?' -> buf = query s0, buf; s0
-           't' -> print_tetramino s0
-           'O' -> %{s0 | tetramino: ['y y ', 'y y ']}
-           'Z' -> %{s0 | tetramino: ['r r . ', '. r r ', '. . . ']}
-           'S' -> %{s0 | tetramino: ['. g g ', 'g g . ', '. . . ']}
-           'J' -> %{s0 | tetramino: ['b . . ', 'b b b ', '. . . ']}
-           'L' -> %{s0 | tetramino: ['. . o ', 'o o o ', '. . . ']}
-           'T' -> %{s0 | tetramino: ['. m . ', 'm m m ', '. . . ']}
+           't' -> print s0.tetramino; s0
+           'O' -> %{s0 | tetramino: ['yy', 'yy']}
+           'Z' -> %{s0 | tetramino: ['rr.', '.rr', '...']}
+           'S' -> %{s0 | tetramino: ['.gg', 'gg.', '...']}
+           'J' -> %{s0 | tetramino: ['b..', 'bbb', '...']}
+           'L' -> %{s0 | tetramino: ['..o', 'ooo', '...']}
+           'T' -> %{s0 | tetramino: ['.m.', 'mmm', '...']}
            _   -> s0 # no change
          end
     unless [ch]=='q', do: io s1, buf
