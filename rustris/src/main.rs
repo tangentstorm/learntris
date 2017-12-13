42e271b5d054c4b18fd9a2aa932815fe75c7d8a1
tangentstorm <michal.wallace@gmail.com>
2017-12-12 21:40:29 -0500

    implement p and q commands (for empty matrix)

--- rustris/src/main.rs
+++ rustris/src/main.rs
@@ -1,3 +1,17 @@
+// use std::io;
+
 fn main() {
-    println!("Hello, world!");
-}
+  let mut s = String::new();
+  std::io::stdin().read_line(&mut s)
+    .expect("failed to read input line.");
+  for c in s.chars() {
+    match c {
+      'q' => break,
+      'p' => print_matrix(),
+      _ => {}
+}}}
+
+fn print_matrix() {
+  for _ in 0..22 {
+    println!(". . . . . . . . . .");
+}}
