7cb2edb5b3e562302ee2bd85c00cc4c57d3a4825
tangentstorm <michal.wallace@gmail.com>
2017-12-13 09:32:48 -0500

    readln and start on g command

--- rustris/src/main.rs
+++ rustris/src/main.rs
@@ -1,17 +1,21 @@
-// use std::io;
+use std::io;
+
+fn readln() -> String {
+  let mut s = String::new();
+  io::stdin().read_line(&mut s)
+    .expect("failed to read input line.");
+  s }
 
 fn main() {
-  let mut s = String::new();
-  std::io::stdin().read_line(&mut s)
-    .expect("failed to read input line.");
-  for c in s.chars() {
-    match c {
-      'q' => break,
-      'p' => print_matrix(),
-      _ => {}
-}}}
+  loop {
+    let s = readln();
+    for c in s.chars() {
+      match c {
+        'q' => return,
+        'p' => print_matrix(),
+        'g' => for _ in 0..22 { readln(); }
+        _ => {} }}}}
 
 fn print_matrix() {
   for _ in 0..22 {
-    println!(". . . . . . . . . .");
-}}
+    println!(". . . . . . . . . ."); }}
