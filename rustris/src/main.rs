80b897ac3e77d191ff9a54a0cc58b79df9f30c93
Michal Wallace <michal.wallace@gmail.com>
2018-01-01 17:43:01 -0500

    make Game.matrix a Sprite

--- rustris/src/main.rs
+++ rustris/src/main.rs
@@ -8,24 +8,27 @@ fn readln() -> String {
     .expect("failed to read input line.");
   s }
 
-fn vec2d<T:Copy>(h:usize, w:usize, fill:T) -> Vec<Vec<T>> {
-  (0..h).map(|_|{ (0..w).map(|_|fill).collect()}).collect() }
-
 
 // -- 2-dimenstonal arrays -------------------------------------
 
 struct Matrix<T> { h: usize, w:usize, data:Vec<T> }
 type Sprite = Matrix<char>;
 
-impl Matrix<char> {
+impl Sprite {
   fn new(h:usize, w:usize, s:&str) -> Self {
     assert_eq!(s.len(), h*w, "s.len() should match h*w");
-    Sprite{ h:h, w:w, data:s.chars().collect() }}
+    Self{ h:h, w:w, data:s.chars().collect() }}
+
+  fn new_fill(h:usize, w:usize, fill:char) -> Self {
+    Self{h:h, w:w, data:(0..h*w).map(|_|fill).collect() }}
 
   fn print(&self) {
     for y in 0..self.h {
       for x in 0..self.w { print!("{} ", self.data[y*self.w + x]) }
       println!("") }}
+
+  fn set(&mut self, y:usize, x:usize, v:char) {
+    self.data[y*self.w + x] = v; }
 
   // rotate clockwise
   fn cw(&self) -> Self {
@@ -33,43 +36,44 @@ impl Matrix<char> {
     for y in 0..self.h { for x in 0..self.w { v.push(self.data[ self.w * (self.h-(x+1)) + y ]) }}
     Self{ h:self.w, w:self.h, data:v }}
 
+  fn hw(&self) -> (usize, usize) { (self.h, self.w) }
+
 }
 
 // -- Game object ---------------------------------------------
 
-struct Game { score: u32, count: u32, matrix : Vec<Vec<char>>, active:Sprite }
+struct Game { score: u32, count: u32, matrix:Sprite, active:Sprite }
 
 impl Game {
   fn new() -> Game {
-    Game { score:0, count:0, matrix: vec2d(22,10,'.'), active:Sprite::new(1,1,"."), }}
+    Game { score:0, count:0, matrix:Sprite::new_fill(22,10,'.'), active:Sprite::new(1,1,"x"), }}
 
   fn clear(&mut self) {
-    self.matrix = vec2d(22,10,'.') }
-
-  fn print_matrix(&self) {
-    for y in 0..22 {
-      for x in 0..10 { print!("{} ", self.matrix[y][x])}
-      println!("")}}
+    self.matrix = Sprite::new_fill(22,10,'.') }
 
   fn get_matrix(&mut self) {
     for y in 0..22 {
       let mut x = 0;
       for c in readln().chars() {
         if c==' ' || c=='\n' {}
-        else { self.matrix[y][x]=c; x+=1 }}}}
+        else { self.matrix.set(y,x,c); x+=1 }}}}
 
   /// step = clear out completed lines
   fn step(&mut self) {
-    let mut cleared = 0;
-    self.matrix = self.matrix.iter().map(|row| {
+    let mut cleared = vec!(); // vector of indices
+    let (h,w) = self.matrix.hw();
+    for y in 0..h {
+      let i = y * w;
+      let row = &self.matrix.data[i..i+w];
       let num_blanks = row.iter().fold(0, |a,c| if *c=='.' {a+1} else {a});
-      if num_blanks == 0 { cleared+=1; vec!['.'; 10 as usize] }
-      else { row.clone() }
-    }).collect();
-    self.count += cleared; self.score += cleared*100;
+      if num_blanks == 0 { cleared.push(y); }}
+    for y in cleared {
+      for x in 0..w { self.matrix.set(y,x,'.') }
+      self.count += 1; self.score += 100;
+    }
   }
-}
 
+}
 // -- command interpreter (main) ------------------------------
 
 fn main() {
@@ -79,7 +83,7 @@ fn main() {
     while let Some(c) = chars.next() {
       match c {
         'q' => return,
-        'p' => g.print_matrix(),
+        'p' => g.matrix.print(),
         'g' => g.get_matrix(),
         'c' => g.clear(),
         's' => g.step(),
